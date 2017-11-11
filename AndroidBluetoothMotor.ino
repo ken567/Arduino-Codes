f36fac96ff58aed4555c4e98f0561b6348c181cd
Kenneth Dulay <kennethdulay@gmail.com>
2017-11-11 21:56:57 +0800

    Add files via upload

---
+++ AndroidBluetoothMotor.ino
@@ -0,0 +1,54 @@
+//MotorDriver Pins
+int pin1 = 8;
+int pin2 = 9;
+int pin3 = 10;
+int pin4 = 11;
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(pin1,OUTPUT);
+  pinMode(pin2,OUTPUT);
+  pinMode(pin3,OUTPUT);
+  pinMode(pin4,OUTPUT);
+}
+
+void loop() {
+  char data;
+  if(Serial.available()>0){
+    data = Serial.read();
+    
+    Serial.println(data);
+    if(data == 'w'){ 
+      Serial.println(data);   
+      digitalWrite(pin1, HIGH);
+      digitalWrite(pin2, HIGH);
+      digitalWrite(pin3, HIGH);
+      digitalWrite(pin4, HIGH);
+    }
+    else if( data == 'q'){
+      Serial.println(data);
+     digitalWrite(pin1, HIGH);
+      digitalWrite(pin2, HIGH);
+      digitalWrite(pin3, HIGH);
+      digitalWrite(pin4, LOW);
+    }
+    else if(data == 'e'){
+      Serial.println(data);
+      digitalWrite(pin1, LOW);
+      digitalWrite(pin2, HIGH);
+      digitalWrite(pin3, HIGH);
+      digitalWrite(pin4, HIGH);  
+    }
+    else if(data == 's'){
+      Serial.println(data);
+      digitalWrite(pin1, LOW);
+      digitalWrite(pin2, HIGH);
+      digitalWrite(pin3, HIGH);
+      digitalWrite(pin4, LOW);
+    }
+    else if (data == '0'){
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+    }
+  }
+}

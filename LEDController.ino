1ca54c69444dea16f8ed44bd46cc395b17fafcdf
Kenneth Dulay <kennethdulay@gmail.com>
2017-11-16 05:50:29 -0800

    Add files via upload

---
+++ LEDController.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char data;
+
+  if(Serial.available()>0){
+    data = Serial.read();
+
+    if(data == '1'){
+      digitalWrite(13,HIGH);
+    }
+    else{
+      digitalWrite(13,LOW);
+    }
+    
+  }
+}
